35bdccbaac5803ca7ba65fe0c5b75bbf7bf46f3d
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-08-22 10:44:15 -0700

    Update config.py
    Hard encoded logging output path & added stdout streaming.

--- Financial-Aid_Post-Capitalism/config.py
+++ Financial-Aid_Post-Capitalism/config.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from datetime import datetime
-import matplotlib.pyplot as plt
 
 class ConfigManager:
     def __init__(self):
@@ -10,7 +9,25 @@ class ConfigManager:
     def configure_logger(self):
         # Configuring the logger with a log file path containing the current timestamp
         log_file_path = 'data/output/logs/preprocessing_log_' + datetime.now().strftime("%d-%m-%Y_%H-%M-%S") + '.log'
-        logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        
+        # Create a file handler that writes log messages to a file
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(logging.INFO)
+        
+        # Create a console handler that writes log messages to stdout
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        
+        # Create a formatter and set it for both handlers
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        
+        # Get the root logger and add both handlers
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
     def check_folder_presence(self, folder_name: str):
         # Checking if the folder exists, and if not, creating it
