58d68aa4d4b5b3090863d69d8bf866ce19b26cea
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-08-22 21:53:45 -0700

    Added docstrings to finaid_train

--- Financial-Aid_Post-Capitalism/finaid_train.py
+++ Financial-Aid_Post-Capitalism/finaid_train.py
@@ -1,3 +1,4 @@
+"""Module to train a Gaussian Mixture Model for financial aid prediction using preprocessed data."""
 from config import ConfigManager
 from datetime import datetime
 from sklearn.mixture import GaussianMixture
@@ -11,6 +12,7 @@ import seaborn as sns
 
 class FinancialAidModelTrainer:
     def __init__(self):
+        """Initialize the FinancialAidModelTrainer with configuration settings and selected features."""
         script_name = "finaid_train"
         self.config_manager = ConfigManager(script_name)
         self.config_manager.setup_configuration()
@@ -19,6 +21,7 @@ class FinancialAidModelTrainer:
         self.model_name = "FinancialAidGMM"
 
     def load_data(self):
+        """Load the latest preprocessed data from CSV file."""
         data_path = self.config_manager.find_latest_preprocessed_file()
         try:
             if data_path:
@@ -33,6 +36,7 @@ class FinancialAidModelTrainer:
             return None
 
     def preprocess_data(self, data):
+        """Preprocess the data by imputing missing values and selecting specific features."""
         features = data[self.selected_features]
         imputer = SimpleImputer(strategy='median')
         features_imputed = imputer.fit_transform(features)
@@ -40,15 +44,15 @@ class FinancialAidModelTrainer:
         return features_imputed
 
     def train_model(self, X_train):
+        """Train the Gaussian Mixture Model using the given training data."""
         model = GaussianMixture(n_components=3)
         model.fit(X_train)
         logging.info('FinancialAidGMM model trained successfully.')
         return model
 
     def visualize_clusters(self, data, clusters):
-        # Ignore the specific UserWarnings related to tight_layout
-        warnings.filterwarnings("ignore", message="The figure layout has changed to tight")
-
+        """Visualize the data clusters using Seaborn pairplot and save the plot as an image."""
+        warnings.filterwarnings("ignore", message="The figure layout has changed to tight") # Ignore the specific UserWarnings related to tight_layout
         data['Cluster'] = clusters
         sns.pairplot(data, hue='Cluster', vars=self.selected_features)
         plt.tight_layout()
@@ -57,11 +61,13 @@ class FinancialAidModelTrainer:
         logging.info(f'Visualization saved as clusters_visualization_{datetime_str}.png.')
 
     def save_model(self, model):
+        """Save the trained Gaussian Mixture Model to a file."""
         model_path = f'data/output/models/{self.model_name}_model.pkl'
         joblib.dump(model, model_path)
         logging.info(f'Model saved as {model_path}.')
 
     def run(self):
+        """Execute the complete training process including loading, preprocessing, training, visualizing, and saving the model."""
         data = self.load_data()
         if data is not None:
             features_imputed = self.preprocess_data(data)
